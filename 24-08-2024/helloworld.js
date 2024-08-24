f0f80f9c73e6f89c276e2b629d9bec8b74927522
Thiago S. Rodrigues <tsr.souza@outlook.com>
2024-08-24 11:52:36 -0300

    Adicionando aula | 24-08-2024

---
+++ 24-08-2024/helloworld.js
@@ -0,0 +1,14 @@
+const number = 2;
+
+const hello = () => {
+
+    console.log("O númer precisa ser igual a 2.");
+
+    if (number == 2) {
+        console.log('Olá, mundo.');
+    } else {
+        console.log("Tente novamente");
+    }
+}
+
+hello();
